ce48122f0ba90e5b51f8e8341a67d1b28fdeaaa8
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-07-31 23:18:11 -0300

    Update aptInstallPhoneHacker.sh

--- Phone-Hacker/aptInstallPhoneHacker.sh
+++ Phone-Hacker/aptInstallPhoneHacker.sh
@@ -2,8 +2,9 @@ apt update && upgrade
 pkg install ncurses-utils
 cat sudo > /data/data/com.termux/files/usr/bin/sudo
 chmod 700 /data/data/com.termux/files/usr/bin/sudo
-apt -y install busybox  git ruby ruby-dev nmap netcat openssh make clang figlet autoconf curl wget nano ncurses-utils hydra libsqlite-dev postgresql postgresql-dev libpcap-dev cmatrix libffi-dev libxslt-dev pkg-config python python-dev python2 python2-dev inetutils
+apt -y install busybox  git ruby ruby-dev nmap netcat openssh make clang figlet autoconf curl wget nano ncurses-utils hydra libsqlite-dev postgresql postgresql-dev libpcap-dev cmatrix libffi-dev libxslt-dev pkg-config python python-dev python2 python2-dev php inetutils
 pip install python-nmap sqlmap
 cd ~
 chmod 777 metasploit@Techzindia.sh
-sudo sh metasploit@Techzindia.sh
+perl -p -i -e "s/\r//g” metasploit.sh
+sudo sh /metasploit@Techzindia.sh
