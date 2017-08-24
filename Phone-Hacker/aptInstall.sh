04b7491262c7a604f70255bd91c134fda336595e
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 11:34:43 -0300

    Delete aptInstall.sh

--- Phone-Hacker/aptInstall.sh
+++
@@ -1,8 +0,0 @@
-apt update && apt -y upgrade
-
-apt -y install apache2 apr apr-util autoconf bison busybox clang coreutils git ruby ruby-dev nmap netcat openssh make figlet autoconf curl wget nano ncurses-utils ncurses-dev hydra sqlite libsqlite-dev postgresql postgresql-dev postgresql-contrib libpcap-dev libgmp-dev cmatrix libffi-dev glib gnupg gnupg2 libxslt-dev libtool libxml2-dev libxslt-dev libgrpc-dev pkg-config python python-dev python2 python2-dev php inetutils findutils ruby-dev readline-dev pip install python-nmap sqlmap openssl tcpdump unzip openssl-dev termux-tools zip w3m
-
-pip install python-nmap sqlmap
-
-
-
