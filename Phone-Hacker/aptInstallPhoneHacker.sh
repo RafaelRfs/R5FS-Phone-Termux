6f837f3a0e4aafefeb8c02b29bbb77e3892df4e2
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 11:59:30 -0300

    Update aptInstallPhoneHacker.sh

--- Phone-Hacker/aptInstallPhoneHacker.sh
+++ Phone-Hacker/aptInstallPhoneHacker.sh
@@ -4,30 +4,30 @@ echo "##############################################"
 echo "______________________________________________"
 echo "[+]Configurando o Termux..."
 echo "______________________________________________"
-apt update && apt -y upgrade
-termux-setup-storage
-pkg install ncurses-utils
-su -c chmod 777 -R .
+apt update && apt -y upgrade  
+termux-setup-storage 
+pkg install ncurses-utils 
+su -c chmod 777 -R . 
 echo "______________________________________________"
-echo "[+]Instando o Sudo..."
+echo "[+]Instando o Sudo..." 
 echo "______________________________________________"
-su -c cat sudo > /data/data/com.termux/files/usr/bin/sudo
-su -c chmod 777 /data/data/com.termux/files/usr/bin/sudo
+su -c cat sudo > /data/data/com.termux/files/usr/bin/sudo 
+su -c chmod 777 /data/data/com.termux/files/usr/bin/sudo 
 echo "______________________________________________"
 echo "[+]Instando o Ngrok..."
 echo "______________________________________________"
 su -c chmod 777 ngrok
-su -c cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
-su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok
+su -c cat ngrok > /data/data/com.termux/files/usr/bin/ngrok  
+su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok  
 echo "______________________________________________"
 echo "[+]Instalando Apps && libs..."
 echo "______________________________________________"
-chmod a+x aptInstallApps.sh
-./aptInstallApps.sh
+chmod a+x aptInstallApps.sh   
+./aptInstallApps.sh    
 echo "______________________________________________"
 echo "[+]Instalando Python Libs..."
 echo "______________________________________________"
-./aptInstallPythonLibs.sh
+./aptInstallPythonLibs.sh   
 echo "______________________________________________"
 echo "[+]Instando o The Harvester..."
 echo "______________________________________________"
@@ -35,30 +35,30 @@ git clone https://github.com/laramies/theHarvester
 echo "______________________________________________"
 echo "[+]Instando o ViSql..."
 echo "______________________________________________"
-./aptInstallViSql.sh
+./aptInstallViSql.sh  
 echo "______________________________________________"
 echo "[+]Instando o Sqlmap..."
 echo "______________________________________________"
-./aptInstallSqlmap
+./aptInstallSqlmap.sh   
 echo "______________________________________________"
 echo "[+]Instalando Youtube-Downloader..."
 echo "______________________________________________"
-./aptInstallYoutube.sh
+./aptInstallYoutube.sh 
 echo "______________________________________________"
 echo "[+]Instalando o Scapy..."
 echo "______________________________________________"
-./aptInstallScapy.sh
+./aptInstallScapy.sh 
 echo "______________________________________________"
 echo "[+]Instalando Metasploit Tech Z India RAPID7..."
 echo "______________________________________________"
-./aptInstallMetasploit.sh
+./aptInstallMetasploit.sh 
 echo "______________________________________________"
 echo "[+]Configurando o PostgreSQL..."
 echo "______________________________________________"
-sudo sh initPstServer.sh
-sudo sh startServerPst.sh
+sudo sh initPstServer.sh 
+sudo sh startServerPst.sh 
 echo "______________________________________________"
 echo "[+]Instalando RouterSploit..."
 echo "______________________________________________"
-./aptInstallRouterSploit.sh
+./aptInstallRouterSploit.sh 
 figlet Phone-Hacker-End
