863de68c8f9e1c29f79c456070daf53e6a248013
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 12:54:36 -0300

    Update aptInstalllFedora.sh

--- Phone-Hacker/aptInstalllFedora.sh
+++ Phone-Hacker/aptInstalllFedora.sh
@@ -1,2 +1,2 @@
 apt update && apt install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh
-sh termux-fedora.sh f25_arm
+./termux-fedora.sh f25_arm
