6ba09d03b0ddf1d4ae1b7eb7676a6b1d5e80148e
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 12:54:57 -0300

    Create aptInstallPhneHackerAdv.sh

---
+++ Phone-Hacker/aptInstallPhneHackerAdv.sh
@@ -0,0 +1,79 @@
+echo "##############################################"
+echo " Phone Hacker RFS ~> Termux/GnuRoot"
+echo "##############################################"
+echo "______________________________________________"
+echo "[+]Configurando o Termux..."
+echo "______________________________________________"
+apt update && apt -y upgrade  
+termux-setup-storage 
+pkg install ncurses-utils 
+echo "______________________________________________"
+echo "[+]Instando o Sudo..." 
+echo "______________________________________________"
+su -c cat sudo > /data/data/com.termux/files/usr/bin/sudo
+su -c chmod 777 /data/data/com.termux/files/usr/bin/sudo
+echo "______________________________________________"
+echo "[+]Instando o Ngrok..."
+echo "______________________________________________"
+su -c cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
+su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok
+echo "______________________________________________"
+echo "[+]Instalando Apps && libs..."
+echo "______________________________________________"
+./aptInstallApps.sh    
+echo "______________________________________________"
+echo "[+]Instalando Python Libs..."
+echo "______________________________________________"
+./aptInstallPythonLibs.sh   
+echo "______________________________________________"
+echo "[+]Instando o The Harvester..."
+echo "______________________________________________"
+git clone https://github.com/laramies/theHarvester
+echo "______________________________________________"
+echo "[+]Instando o ViSql..."
+echo "______________________________________________"
+./aptInstallViSql.sh  
+echo "______________________________________________"
+echo "[+]Instando o Sqlmap..."
+echo "______________________________________________"
+./aptInstallSqlmap.sh   
+echo "______________________________________________"
+echo "[+]Instalando Youtube-Downloader..."
+echo "______________________________________________"
+./aptInstallYoutube.sh 
+echo "______________________________________________"
+echo "[+]Instalando o Scapy..."
+echo "______________________________________________"
+./aptInstallScapy.sh 
+echo "______________________________________________"
+echo "[+]Instalando Metasploit Tech Z India RAPID7..."
+echo "______________________________________________"
+./metasploit@Techzindia.sh
+echo "______________________________________________"
+echo "[+]Configurando o PostgreSQL..."
+echo "______________________________________________"
+./initPstServer.sh 
+./startServerPst.sh 
+echo "______________________________________________"
+echo "[+]Instalando RouterSploit..."
+echo "______________________________________________"
+./aptInstallRouterSploit.sh 
+echo "______________________________________________"
+echo "[+]Instalando modulos Advance"
+echo "______________________________________________"
+echo "[+]Instalando o Fedora Linux..."
+echo "______________________________________________"
+./aptInstalllFedora.sh
+echo "______________________________________________"
+echo "[+]Instalando o Wine..."
+echo "______________________________________________"
+startfedora
+dnf install wine
+dnf install ettercap
+dnf install iptables
+dnf install sslstrip
+
+
+echo "##############################################"
+echo "[+]PHONE-HACKER Advanced Success"
+echo "##############################################"
