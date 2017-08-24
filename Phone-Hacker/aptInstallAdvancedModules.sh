445fe4cf6ee6582b44d806c9cdc3edad94513939
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 11:44:39 -0300

    Update aptInstallAdvancedModules.sh

--- Phone-Hacker/aptInstallAdvancedModules.sh
+++ Phone-Hacker/aptInstallAdvancedModules.sh
@@ -1,12 +1,9 @@
 echo "______________________________________________"
 echo "[+]Instalando o Fedora Linux..."
 echo "______________________________________________"
-sh aptInstalllFedora.sh
-sleep 60
-
+./aptInstalllFedora.sh
 echo "______________________________________________"
 echo "[+]Instalando o Wine..."
 echo "______________________________________________"
 startfedora
 dnf install wine
-sleep 60
