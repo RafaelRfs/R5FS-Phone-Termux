b91aad1bd25429c73a4d95f3d9de13cea1ecdfd3
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 12:38:41 -0300

    Update aptInstallYoutube.sh

--- Phone-Hacker/aptInstallYoutube.sh
+++ Phone-Hacker/aptInstallYoutube.sh
@@ -1,15 +1,4 @@
-apt update && apt -y upgrade
-
-
-termux-setup-storage
-
-
-sudo wget http://pastebin.com/raw/W1nvzN6q -O install.sh
-
-sudo chmod 777 install.sh
-
+su -c wget http://pastebin.com/raw/W1nvzN6q -O install.sh
 dos2unix install.sh
-
-sudo chmod u+x install.sh
-
-sudo ./install.sh
+su -c chmod u+x install.sh
+su -c ./install.sh
