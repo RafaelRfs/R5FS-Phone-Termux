06130e01d2393c0b9c88550fc488d959e9bdc56b
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 11:42:57 -0300

    Update aptInstallApps.sh

--- Phone-Hacker/aptInstallApps.sh
+++ Phone-Hacker/aptInstallApps.sh
@@ -1,6 +1,2 @@
 apt update && apt -y upgrade
-
 apt install -y apache2 apr apr-util autoconf bison busybox clang coreutils git ruby ruby-dev nmap netcat openssh make figlet autoconf curl wget nano ncurses-utils ncurses-dev hydra sqlite libsqlite-dev postgresql postgresql-dev postgresql-contrib libpcap-dev libgmp-dev cmatrix libffi-dev glib gnupg gnupg2 libxslt-dev libtool libxml2-dev libxslt-dev libgrpc-dev pkg-config python python-dev python2 python2-dev php inetutils findutils readline-dev  openssl tcpdump unzip openssl-dev termux-tools termux-api zip w3m
-
-pip install python-nmap sqlmap colorama PyX requests setuptools urllib3 wifi
-pip2 install BeautifulSoup pycparser six urllib3 wifi youtube-dl
