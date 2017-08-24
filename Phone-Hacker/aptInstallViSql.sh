b1832b00c1c40e274dee430df70fa6fc9d66f416
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-24 12:36:42 -0300

    Update aptInstallViSql.sh

--- Phone-Hacker/aptInstallViSql.sh
+++ Phone-Hacker/aptInstallViSql.sh
@@ -1,10 +1,3 @@
-apt update && upgrade
-
-apt install git
-
 git clone https://github.com/blackvkng/viSQL
-
+cd ViSQL
 python2 -m pip install -r requirements.txt
-
-cd viSQL
-python2 viSQL.py --help
