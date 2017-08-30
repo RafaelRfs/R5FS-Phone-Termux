4580a92f4ee9c17c0a6ddb7cc904009b49b805a6
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-29 23:17:40 -0300

    Update initPstServer.sh

--- Phone-Hacker/initPstServer.sh
+++ Phone-Hacker/initPstServer.sh
@@ -1,3 +1,6 @@
 initdb ~/pg
 pgctl -D ~/pg start
 psql -l
+echo "[+]Configurando o Php Ini"
+su -c cat php.ini > /data/data/com.termux/files/usr/lib/php.ini
+su -c chmod 777 /data/data/com.termux/files/usr/lib/php.ini
