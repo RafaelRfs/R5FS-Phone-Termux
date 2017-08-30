ed1c09a7f79e2f055f94bb8dc12a6bf9a862cf22
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-08-30 10:54:13 -0300

    Update aptInstallBin.sh

--- Phone-Hacker/aptInstallBin.sh
+++ Phone-Hacker/aptInstallBin.sh
@@ -1,12 +1,13 @@
 echo "[+]Instalando binarios..."
-su -c cat Client > /data/data/com.termux/files/usr/bin/Client
-su -c cat Server > /data/data/com.termux/files/usr/bin/Server
-su -c cat mit > /data/data/com.termux/files/usr/bin/mit
-su -c cat msfconsole > /data/data/com.termux/files/usr/bin/msfconsole
-su -c cat network > /data/data/com.termux/files/usr/bin/network
-su -c cat network-nmap > /data/data/com.termux/files/usr/bin/network-nmap
-su -c cat rsf > /data/data/com.termux/files/usr/bin/rsf
-su -c cat showifi > /data/data/com.termux/files/usr/bin/showifi
+su -c mv Client  /data/data/com.termux/files/usr/bin/Client
+su -c mv Server /data/data/com.termux/files/usr/bin/Server
+su -c mv mit  /data/data/com.termux/files/usr/bin/mit
+su -c mv msfconsole /data/data/com.termux/files/usr/bin/msfconsole
+su -c mv network  /data/data/com.termux/files/usr/bin/network
+su -c mv network-nmap /data/data/com.termux/files/usr/bin/network-nmap
+su -c mv rsf /data/data/com.termux/files/usr/bin/rsf
+su -c mv showifi /data/data/com.termux/files/usr/bin/showifi
+su -c mv verser /data/data/com.termux/files/usr/bin/verser
 
 echo "[+]Binary permissions: "
 su -c chmod 777 /data/data/com.termux/files/usr/bin/Client
@@ -17,3 +18,4 @@ su -c chmod 777 /data/data/com.termux/files/usr/bin/network
 su -c chmod 777 /data/data/com.termux/files/usr/bin/network-nmap
 su -c chmod 777 /data/data/com.termux/files/usr/bin/rsf
 su -c chmod 777 /data/data/com.termux/files/usr/bin/showifi
+su -c chmod 777 /data/data/com.termux/files/usr/bin/verser
