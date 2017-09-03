359f049068625ac6f3a8f422edea7c4e81177b3d
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-09-03 11:56:31 -0300

    Update initPstServer.sh

--- Phone-Hacker/initPstServer.sh
+++ Phone-Hacker/initPstServer.sh
@@ -2,6 +2,6 @@ initdb pg
 pg_ctl -D pg start
 psql -d postgres -f user.sql
 echo "[+]Configurando o Php Ini"
-su -c mv php.ini /data/data/com.termux/files/usr/lib/php.ini
+su -c mv php.ini /data/data/com.termux/files/usr/lib/
 su -c chmod 777 /data/data/com.termux/files/usr/lib/php.ini
 echo "[+]Configure o arquivo index-pdo.php, monte o server php e acesse o arquivo[index-pdo.php]"
