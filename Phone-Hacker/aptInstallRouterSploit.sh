83acd3cefa9b6ffc6f974783ee1dcb47fbb34923
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-09-03 11:46:39 -0300

    Update aptInstallRouterSploit.sh

--- Phone-Hacker/aptInstallRouterSploit.sh
+++ Phone-Hacker/aptInstallRouterSploit.sh
@@ -1,4 +1,5 @@
-su -c apt install python-dev python-pip libncurses5-dev git
+echo "[+++]Routersploit in your system..."
+apt install python-dev python-pip libncurses5-dev git
 git clone https://github.com/reverse-shell/routersploit
 cd routersploit
 pip install -r requirements.txt
