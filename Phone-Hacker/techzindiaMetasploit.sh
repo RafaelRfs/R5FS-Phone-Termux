f956e64e16037f6a0f898cf1dc20ec3d16875ec6
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-09-03 00:15:56 -0300

    Update techzindiaMetasploit.sh

--- Phone-Hacker/techzindiaMetasploit.sh
+++ Phone-Hacker/techzindiaMetasploit.sh
@@ -24,7 +24,7 @@ gem install bundler
 
 #Install nokogiri
 echo "nokogiri is installing......"
-gem install nokogiri -- --use-system-libraries
+sudo gem install nokogiri -- --use-system-libraries
 
 #Install Network-Interface
 
@@ -33,9 +33,9 @@ cd network_interface-0.0.1
 sed 's|git ls-files|find -type f|' -i network_interface.gemspec
 curl -L https://wiki.termux.com/images/6/6b/Netifaces.patch -o netifaces.patch
 patch -p1 < netifaces.patch
-gem build network_interface.gemspec
+sudo gem build network_interface.gemspec
 echo "network_interface is installing........"
-gem install network_interface-0.0.1.gem
+sudo gem install network_interface-0.0.1.gem
 cd ..
 rm -r network_interface-0.0.1
 
