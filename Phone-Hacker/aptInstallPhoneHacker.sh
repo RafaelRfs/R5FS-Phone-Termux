261a6d64a22dfabf17c412b2d098c3cdc2818111
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-09-03 11:13:50 -0300

    Update aptInstallPhoneHacker.sh

--- Phone-Hacker/aptInstallPhoneHacker.sh
+++ Phone-Hacker/aptInstallPhoneHacker.sh
@@ -4,61 +4,77 @@ echo "##############################################"
 echo "______________________________________________"
 echo "[+]Configurando o Termux..."
 echo "______________________________________________"
+dir_bin=/data/data/com.termux/files/usr/bin/
 apt update && apt -y upgrade  
 termux-setup-storage 
 pkg install ncurses-utils 
+su -c chmod 777 *
+sleep 5
 echo "______________________________________________"
 echo "[+]Instando o Sudo..." 
 echo "______________________________________________"
-su -c mv sudo  /data/data/com.termux/files/usr/bin/sudo
+su -c mv sudo $dir_bin
 su -c chmod 777 /data/data/com.termux/files/usr/bin/sudo
+sleep 5
 echo "______________________________________________"
 echo "[+]Instando o Ngrok..."
 echo "______________________________________________"
-su -c mv ngrok /data/data/com.termux/files/usr/bin/ngrok
+su -c mv ngrok $dir_bin
 su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok
+sleep 5
 echo "______________________________________________"
 echo "[+]Instalando Apps && libs..."
 echo "______________________________________________"
 ./aptInstallApps.sh
+sleep 5
 ./aptInstallBin.sh
+sleep 5
 echo "______________________________________________"
 echo "[+]Instalando Python Libs..."
 echo "______________________________________________"
 ./aptInstallPythonLibs.sh   
+sleep 5
 echo "______________________________________________"
 echo "[+]Instando o The Harvester..."
 echo "______________________________________________"
 git clone https://github.com/laramies/theHarvester
+sleep 5
 echo "______________________________________________"
 echo "[+]Instando o ViSql..."
 echo "______________________________________________"
 ./aptInstallViSql.sh  
+sleep 5
 echo "______________________________________________"
 echo "[+]Instando o Sqlmap..."
 echo "______________________________________________"
 ./aptInstallSqlmap.sh   
+sleep 5
 echo "______________________________________________"
 echo "[+]Instalando Youtube-Downloader..."
 echo "______________________________________________"
 ./aptInstallYoutube.sh 
+sleep 5
 echo "______________________________________________"
 echo "[+]Instalando o Scapy..."
 echo "______________________________________________"
 ./aptInstallScapy.sh 
+sleep 5
 echo "______________________________________________"
 echo "[+]Instalando Metasploit Tech Z India RAPID7..."
 echo "______________________________________________"
 ./metasploit@Techzindia.sh
+sleep 5
 echo "______________________________________________"
 echo "[+]Configurando o PostgreSQL..."
 echo "______________________________________________"
 ./initPstServer.sh 
 ./startServerPst.sh 
+sleep 5
 echo "______________________________________________"
 echo "[+]Instalando RouterSploit..."
 echo "______________________________________________"
 ./aptInstallRouterSploit.sh 
+sleep 5
 echo "______________________________________________"
 echo "##############################################"
 echo "[+]PHONE-HACKER Success"
