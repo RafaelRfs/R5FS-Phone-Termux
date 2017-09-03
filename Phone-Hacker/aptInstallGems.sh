047c420eef925effe3ca3938f58bc7927e082136
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-09-02 23:36:30 -0300

    Add files via upload

---
+++ Phone-Hacker/aptInstallGems.sh
@@ -0,0 +1,253 @@
+﻿sudo gem install Ascii85 -v 1.0.2
+sudo gem install actionpack -v 4.2.9
+sudo gem install actionview -v 4.2.9
+sudo gem install activesupport -v 4.2.9
+sudo gem install rack -v 1.6
+sudo gem install rack-test -v 0.6.2
+sudo gem install rails-dom-testing -v 1.0
+sudo gem install rails-html-sanitizer -v 1.0
+sudo gem install actionview -v 4.2.9
+sudo gem install activesupport -v 4.2.9
+sudo gem install builder -v 3.1
+sudo gem install erubis -v 2.7.0
+sudo gem install rails-dom-testing -v 1.0
+sudo gem install rails-html-sanitizer -v 1.0
+sudo gem install activemodel -v 4.2.9
+sudo gem install activesupport -v 4.2.9
+sudo gem install builder -v 3.1
+sudo gem install activerecord -v 4.2.9
+sudo gem install activemodel -v 4.2.9
+sudo gem install activesupport -v 4.2.9
+sudo gem install arel -v 6.0
+sudo gem install activesupport -v 4.2.9
+sudo gem install i18n -v 0.7
+sudo gem install minitest -v 5.1
+sudo gem install thread_safe -v 0.3
+sudo gem install tzinfo -v 1.1
+sudo gem install addressable -v 2.5.2
+sudo gem install public_suffix -v 2.0.2
+sudo gem install afm -v 0.2.2
+sudo gem install arel -v 6.0.4
+sudo gem install arel-helpers -v 2.4.0
+sudo gem install activerecord -v 3.1.0
+sudo gem install backports -v 3.8.0
+sudo gem install bcrypt -v 3.1.11
+sudo gem install bcrypt_pbkdf -v 1.0.0
+sudo gem install bindata -v 2.4.1
+sudo gem install bit-struct -v 0.16
+sudo gem install builder -v 3.2.3
+sudo gem install coderay -v 1.1.1
+sudo gem install diff-lcs -v 1.3
+sudo gem install dnsruby -v 1.60.2
+sudo gem install docile -v 1.1.5
+sudo gem install erubis -v 2.7.0
+sudo gem install factory_girl -v 4.8.0
+sudo gem install activesupport -v 3.0.0
+sudo gem install factory_girl_rails -v 4.8.0
+sudo gem install factory_girl -v 4.8.0
+sudo gem install railties -v 3.0.0
+sudo gem install faraday -v 0.13.1
+sudo gem install multipart-post -v 1.2
+sudo gem install ffi -v 1.9.18
+sudo gem install filesize -v 0.1.1
+sudo gem install fivemat -v 1.3.5
+sudo gem install hashery -v 2.1.2
+sudo gem install i18n -v 0.8.6
+sudo gem install jsobfu -v 0.4.2
+sudo gem install rkelly-remix
+sudo gem install json -v 2.1.0
+sudo gem install loofah -v 2.0.3
+sudo gem install nokogiri -v 1.5.9
+sudo gem install metasm -v 1.0.3
+sudo gem install metasploit-concern -v 2.0.5
+sudo gem install activemodel -v 4.2.6
+sudo gem install activesupport -v 4.2.6
+sudo gem install railties -v 4.2.6
+sudo gem install metasploit-credential -v 2.0.12
+sudo gem install metasploit-concern
+sudo gem install metasploit-model
+sudo gem install metasploit_data_models
+sudo gem install pg
+sudo gem install railties
+sudo gem install rex-socket
+sudo gem install rubyntlm
+sudo gem install rubyzip
+sudo gem install metasploit-model -v 2.0.4
+sudo gem install activemodel -v 4.2.6
+sudo gem install activesupport -v 4.2.6
+sudo gem install railties -v 4.2.6
+sudo gem install metasploit-payloads -v 1.3.1
+sudo gem install metasploit_data_models -v 2.0.15
+sudo gem install activerecord -v 4.2.6
+sudo gem install activesupport -v 4.2.6
+sudo gem install arel-helpers
+sudo gem install metasploit-concern
+sudo gem install metasploit-model
+sudo gem install pg
+sudo gem install postgres_ext
+sudo gem install railties -v 4.2.6
+sudo gem install recog -v 2.0
+sudo gem install metasploit_payloads-mettle -v 0.2.2
+sudo gem install method_source -v 0.8.2
+sudo gem install mini_portile2 -v 2.2.0
+sudo gem install minitest -v 5.10.3
+sudo gem install msgpack -v 1.1.0
+sudo gem install multipart-post -v 2.0.0
+sudo gem install nessus_rest -v 0.1.6
+sudo gem install net-ssh -v 4.1.0
+sudo gem install network_interface -v 0.0.2
+sudo gem install nexpose -v 7.0.0
+sudo gem install nokogiri -v 1.8.0
+sudo gem install mini_portile2 -v 2.2.0
+sudo gem install octokit -v 4.7.0
+sudo gem install sawyer -v 0.8.0
+sudo gem install openssl-ccm -v 1.2.1
+sudo gem install openvas-omp -v 0.0.4
+sudo gem install packetfu -v 1.1.13
+sudo gem install pcaprub
+sudo gem install patch_finder -v 1.0.2
+sudo gem install pcaprub -v 0.12.4
+sudo gem install pdf-reader -v 2.0.0
+sudo gem install Ascii85 -v 1.0.0
+sudo gem install afm -v 0.2.1
+sudo gem install hashery -v 2.0
+sudo gem install ruby-rc4
+sudo gem install ttfunk
+sudo gem install pg -v 0.20.0
+sudo gem install pg_array_parser -v 0.0.9
+sudo gem install postgres_ext -v 3.0.0
+sudo gem install activerecord -v 4.0.0
+sudo gem install arel -v 4.0.1
+sudo gem install pg_array_parser -v 0.0.9
+sudo gem install pry -v 0.10.4
+sudo gem install coderay -v 1.1.0
+sudo gem install method_source -v 0.8.1
+sudo gem install slop -v 3.4
+sudo gem install public_suffix -v 3.0.0
+sudo gem install rack -v 1.6.8
+sudo gem install rack-test -v 0.6.3
+sudo gem install rack -v 1.0
+sudo gem install rails-deprecated_sanitizer -v 1.0.3
+sudo gem install activesupport -v 4.2.0.alpha
+sudo gem install rails-dom-testing -v 1.0.8
+sudo gem install activesupport -v 4.2.0.beta
+sudo gem install nokogiri -v 1.6
+sudo gem install rails-deprecated_sanitizer -v 1.0.1
+sudo gem install rails-html-sanitizer -v 1.0.3
+sudo gem install loofah -v 2.0
+sudo gem install railties -v 4.2.9
+sudo gem install actionpack -v 4.2.9
+sudo gem install activesupport -v 4.2.9
+sudo gem install rake -v 0.8.7
+sudo gem install thor -v 0.18.1
+sudo gem install rake -v 12.0.0
+sudo gem install rb-readline -v 0.5.5
+sudo gem install rbnacl -v 4.0.2
+sudo gem install ffi
+sudo gem install rbnacl-libsodium -v 1.0.13
+sudo gem install rbnacl -v 3.0.1
+sudo gem install recog -v 2.1.13
+sudo gem install nokogiri
+sudo gem install redcarpet -v 3.4.0
+sudo gem install rex-arch -v 0.1.11
+sudo gem install rex-text
+sudo gem install rex-bin_tools -v 0.1.4
+sudo gem install metasm
+sudo gem install rex-arch
+sudo gem install rex-core
+sudo gem install rex-struct2
+sudo gem install rex-text
+sudo gem install rex-core -v 0.1.12
+sudo gem install rex-encoder -v 0.1.4
+sudo gem install metasm
+sudo gem install rex-arch
+sudo gem install rex-text
+sudo gem install rex-exploitation -v 0.1.14
+sudo gem install jsobfu
+sudo gem install metasm
+sudo gem install rex-arch
+sudo gem install rex-encoder
+sudo gem install rex-text
+sudo gem install rex-java -v 0.1.5
+sudo gem install rex-mime -v 0.1.5
+sudo gem install rex-text
+sudo gem install rex-nop -v 0.1.1
+sudo gem install rex-arch
+sudo gem install rex-ole -v 0.1.6
+sudo gem install rex-text
+sudo gem install rex-powershell -v 0.1.72
+sudo gem install rex-random_identifier
+sudo gem install rex-text
+sudo gem install rex-random_identifier -v 0.1.3
+sudo gem install rex-text
+sudo gem install rex-registry -v 0.1.3
+sudo gem install rex-rop_builder -v 0.1.3
+sudo gem install metasm
+sudo gem install rex-core
+sudo gem install rex-text
+sudo gem install rex-socket -v 0.1.8
+sudo gem install rex-core
+sudo gem install rex-sslscan -v 0.1.5
+sudo gem install rex-core
+sudo gem install rex-socket
+sudo gem install rex-text
+sudo gem install rex-struct2 -v 0.1.2
+sudo gem install rex-text -v 0.2.15
+sudo gem install rex-zip -v 0.1.3
+sudo gem install rex-text
+sudo gem install rkelly-remix -v 0.0.7
+sudo gem install rspec -v 3.6.0
+sudo gem install rspec-core -v 3.6.0
+sudo gem install rspec-expectations -v 3.6.0
+sudo gem install rspec-mocks -v 3.6.0
+sudo gem install rspec-core -v 3.6.0
+sudo gem install rspec-support -v 3.6.0
+sudo gem install rspec-expectations -v 3.6.0
+sudo gem install diff-lcs -v 1.2.0
+sudo gem install rspec-support -v 3.6.0
+sudo gem install rspec-mocks -v 3.6.0
+sudo gem install diff-lcs -v 1.2.0
+sudo gem install rspec-support -v 3.6.0
+sudo gem install rspec-rails -v 3.6.1
+sudo gem install actionpack -v 3.0
+sudo gem install activesupport -v 3.0
+sudo gem install railties -v 3.0
+sudo gem install rspec-core -v 3.6.0
+sudo gem install rspec-expectations -v 3.6.0
+sudo gem install rspec-mocks -v 3.6.0
+sudo gem install rspec-support -v 3.6.0
+sudo gem install rspec-rerun -v 1.1.0
+sudo gem install rspec -v 3.0
+sudo gem install rspec-support -v 3.6.0
+sudo gem install ruby-rc4 -v 0.1.5
+sudo gem install ruby_smb -v 0.0.18
+sudo gem install bindata
+sudo gem install rubyntlm
+sudo gem install windows_error
+sudo gem install rubyntlm -v 0.6.2
+sudo gem install rubyzip -v 1.2.1
+sudo gem install sawyer -v 0.8.1
+sudo gem install addressable -v 2.3.5
+sudo gem install faraday -v 0.8
+sudo gem install simplecov -v 0.15.0
+sudo gem install docile -v 1.1.0
+sudo gem install json -v 1.8
+sudo gem install simplecov-html -v 0.10.0
+sudo gem install simplecov-html -v 0.10.2
+sudo gem install slop -v 3.6.0
+sudo gem install sqlite3 -v 1.3.13
+sudo gem install sshkey -v 1.9.0
+sudo gem install thor -v 0.20.0
+sudo gem install thread_safe -v 0.3.6
+sudo gem install timecop -v 0.9.1
+sudo gem install ttfunk -v 1.5.1
+sudo gem install tzinfo -v 1.2.3
+sudo gem install thread_safe -v 0.1
+sudo gem install tzinfo-data -v 1.2017.2
+sudo gem install tzinfo -v 1.0.0
+sudo gem install windows_error -v 0.1.2
+sudo gem install xdr -v 2.0.0
+sudo gem install activemodel -v 4.2.7
+sudo gem install activesupport -v 4.2.7
+sudo gem install xmlrpc -v 0.3.0
+sudo gem install yard -v 0.9.9
