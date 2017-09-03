4ed25ed6fbd5125dfdb5549d9eff33d7f5e401a2
Rafael Rfs <RafaelRfs@users.noreply.github.com>
2017-09-03 12:29:34 -0300

    Update aptInstallPhoneHacker.sh

--- Phone-Hacker/aptInstallPhoneHacker.sh
+++ Phone-Hacker/aptInstallPhoneHacker.sh
@@ -1,5 +1,5 @@
 echo "##############################################"
-echo " Phone Hacker RFS ~> Termux/GnuRoot"
+echo " Phone Hacker R5FS ~> Termux/GnuRoot"
 echo "##############################################"
 echo "______________________________________________"
 echo "[+]Configurando o Termux..."
@@ -9,18 +9,6 @@ apt update && apt -y upgrade
 termux-setup-storage 
 pkg install ncurses-utils 
 su -c chmod 777 *
-sleep 5
-echo "______________________________________________"
-echo "[+]Instando o Sudo..." 
-echo "______________________________________________"
-su -c mv sudo $dir_bin
-su -c chmod 777 /data/data/com.termux/files/usr/bin/sudo
-sleep 5
-echo "______________________________________________"
-echo "[+]Instando o Ngrok..."
-echo "______________________________________________"
-su -c mv ngrok $dir_bin
-su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok
 sleep 5
 echo "______________________________________________"
 echo "[+]Instalando Apps && libs..."
@@ -33,6 +21,29 @@ echo "______________________________________________"
 echo "[+]Instalando Python Libs..."
 echo "______________________________________________"
 ./aptInstallPythonLibs.sh   
+sleep 5
+echo "______________________________________________"
+echo "[+]Instalando o Scapy..."
+echo "______________________________________________"
+./aptInstallScapy.sh 
+sleep 5
+echo "______________________________________________"
+echo "[+]Configurando o PostgreSQL..."
+echo "______________________________________________"
+./initPstServer.sh 
+./startServerPst.sh 
+sleep 5
+echo "______________________________________________"
+echo "[+]Instando o Sudo..." 
+echo "______________________________________________"
+su -c mv sudo $dir_bin
+su -c chmod 777 /data/data/com.termux/files/usr/bin/sudo
+sleep 5
+echo "______________________________________________"
+echo "[+]Instando o Ngrok..."
+echo "______________________________________________"
+su -c mv ngrok $dir_bin
+su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok
 sleep 5
 echo "______________________________________________"
 echo "[+]Instando o The Harvester..."
@@ -55,9 +66,9 @@ echo "______________________________________________"
 ./aptInstallYoutube.sh 
 sleep 5
 echo "______________________________________________"
-echo "[+]Instalando o Scapy..."
+echo "[+]Instalando RouterSploit..."
 echo "______________________________________________"
-./aptInstallScapy.sh 
+./aptInstallRouterSploit.sh 
 sleep 5
 echo "______________________________________________"
 echo "[+]Instalando Metasploit Tech Z India RAPID7..."
@@ -65,17 +76,7 @@ echo "______________________________________________"
 ./metasploit@Techzindia.sh
 sleep 5
 echo "______________________________________________"
-echo "[+]Configurando o PostgreSQL..."
-echo "______________________________________________"
-./initPstServer.sh 
-./startServerPst.sh 
-sleep 5
-echo "______________________________________________"
-echo "[+]Instalando RouterSploit..."
-echo "______________________________________________"
-./aptInstallRouterSploit.sh 
-sleep 5
-echo "______________________________________________"
 echo "##############################################"
 echo "[+]PHONE-HACKER Success"
 echo "##############################################"
+sleep 5
