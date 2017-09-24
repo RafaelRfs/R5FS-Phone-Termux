7266785260d6d99ad2cbdb07fbb484b2e1ac9596
R5fs <RafaelRfs@users.noreply.github.com>
2017-09-24 17:49:03 -0300

    Update aptInstallPhoneHacker.sh

--- Phone-Hacker/aptInstallPhoneHacker.sh
+++ Phone-Hacker/aptInstallPhoneHacker.sh
@@ -6,17 +6,15 @@ echo "[+]Configurando o Termux..."
 echo "______________________________________________"
 dir_bin=/data/data/com.termux/files/usr/bin/
 apt update && apt -y upgrade  
-
 termux-setup-storage 
 pkg install ncurses-utils 
 su -c dos2unix *.sh
 su -c chmod 777 *
 sleep 5
 echo "______________________________________________"
-echo "[+]Instalando Apps && libs..."
+echo "[+]Installing Apps && libs..."
 echo "______________________________________________"
 ./aptInstallApps.sh
-sleep 5
 ./aptInstallBin.sh
 sleep 5
 echo "______________________________________________"
@@ -46,39 +44,7 @@ echo "[+]Instando o Ngrok..."
 echo "______________________________________________"
 su -c cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
 su -c chmod 777 /data/data/com.termux/files/usr/bin/ngrok
-sleep 5
-echo "______________________________________________"
-echo "[+]Instando o The Harvester..."
-echo "______________________________________________"
-git clone https://github.com/laramies/theHarvester
-sleep 5
-echo "______________________________________________"
-echo "[+]Instando o ViSql..."
-echo "______________________________________________"
-./aptInstallViSql.sh  
-sleep 5
-echo "______________________________________________"
-echo "[+]Instando o Sqlmap..."
-echo "______________________________________________"
-./aptInstallSqlmap.sh   
-sleep 5
-echo "______________________________________________"
-echo "[+]Instalando Youtube-Downloader..."
-echo "______________________________________________"
-./aptInstallYoutube.sh 
-sleep 5
-echo "______________________________________________"
-echo "[+]Instalando RouterSploit..."
-echo "______________________________________________"
-./aptInstallRouterSploit.sh 
-sleep 5
-echo "______________________________________________"
-echo "[+]Instalando Metasploit Tech Z India RAPID7..."
-echo "______________________________________________"
-./metasploit@Techzindia.sh
-sleep 5
 echo "______________________________________________"
 echo "##############################################"
 echo "[+]PHONE-HACKER Success"
 echo "##############################################"
-sleep 5
