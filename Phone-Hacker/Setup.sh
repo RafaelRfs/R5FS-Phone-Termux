82cdb450030e53c958d0ead32d12c8a89cbb02bc
R5fs <RafaelRfs@users.noreply.github.com>
2017-10-10 11:46:54 -0300

    Update Setup.sh

--- Phone-Hacker/Setup.sh
+++ Phone-Hacker/Setup.sh
@@ -1,5 +1,9 @@
 su -c dos2unix *.sh
 su -c chmod 777 -R *
 ./aptInstallPhoneHacker.sh
+echo "[+]Installing Programs..."
 ./aptInstallPrograms.sh
+echo "[+]Installing Kali..."
 ./aptInstallKali.sh
+echo "[+]Installing Fedora..."
+./aptInstalllFedora.sh
